caae75b071947b56ef99416519ef978afd3020c5
Jack McKew <jackmckew2@gmail.com>
2020-09-12 17:03:42 +1000

    Update image tags

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,16 @@
-docker build -t jackmckew/multi-client:latest -t jackmckew/multi-client:${GITHUB_SHA} -f ./client/Dockerfile ./client
-docker build -t jackmckew/multi-server:latest -t jackmckew/multi-server:${GITHUB_SHA} -f ./server/Dockerfile ./server
-docker build -t jackmckew/multi-worker:latest -t jackmckew/multi-worker:${GITHUB_SHA} -f ./worker/Dockerfile ./worker
+docker build -t jackmckew/multi-docker-client:latest -t jackmckew/multi-docker-client:${GITHUB_SHA} -f ./client/Dockerfile ./client
+docker build -t jackmckew/multi-docker-server:latest -t jackmckew/multi-docker-server:${GITHUB_SHA} -f ./server/Dockerfile ./server
+docker build -t jackmckew/multi-docker-worker:latest -t jackmckew/multi-docker-worker:${GITHUB_SHA} -f ./worker/Dockerfile ./worker
 
-docker push jackmckew/multi-client:latest
-docker push jackmckew/multi-server:latest
-docker push jackmckew/multi-worker:latest
+docker push jackmckew/multi-docker-client:latest
+docker push jackmckew/multi-docker-server:latest
+docker push jackmckew/multi-docker-worker:latest
 
-docker push jackmckew/multi-client:${GITHUB_SHA}
-docker push jackmckew/multi-server:${GITHUB_SHA}
-docker push jackmckew/multi-worker:${GITHUB_SHA}
+docker push jackmckew/multi-docker-client:${GITHUB_SHA}
+docker push jackmckew/multi-docker-server:${GITHUB_SHA}
+docker push jackmckew/multi-docker-worker:${GITHUB_SHA}
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=jackmckew/multi-client:${GITHUB_SHA}
-kubectl set image deployments/server-deployment server=jackmckew/multi-server:${GITHUB_SHA}
-kubectl set image deployments/worker-deployment worker=jackmckew/multi-worker:${GITHUB_SHA}
+kubectl set image deployments/client-deployment client=jackmckew/multi-docker-client:${GITHUB_SHA}
+kubectl set image deployments/server-deployment server=jackmckew/multi-docker-server:${GITHUB_SHA}
+kubectl set image deployments/worker-deployment worker=jackmckew/multi-docker-worker:${GITHUB_SHA}
